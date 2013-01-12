81c102fa28a74a5dd2068d29910512da64c1c029
Hazel <hcourt@uchicago.edu>
2013-01-11 18:59:08 -0600

    1/11/2013

--- Scraper.py
+++ Scraper.py
@@ -6,6 +6,7 @@ def email (new):
 def saveToServer (new):
     print "page has been saved";
     return;
+
 current = urllib.urlopen('http://www.bloomberg.com/view/bios/jonathan-alter');\
 old = urllib.urlopen('http://www.bloomberg.com/view/bios/jonathan-alter');
 prevC = '';
@@ -14,8 +15,10 @@ for (lineC, lineO) in zip (current, old):
         if prevC == '<h3><a class="author" href="/view/bios/jonathan-alter/">Jonathan Alter</a></h3>':
             #<h2><a href="/news/2013-01-03/liberals-nip-obama-as-he-battles-republicans.html" class="q">Liberals Nip Obama as He Battles Republicans</a></h2>
             if lineC != lineO:
-                print lineC;
-                print lineO;
+                link = lineC[(lineC.find('/news/')) : (lineC.find('" class=') - 1)];
+                title = lineC[(lineC.find('"q">') + 4) : (lineC.find('</a>'))];
+                print title;
+                print link;
                 email(lineC);
                 saveToServer(current);
                 break;
